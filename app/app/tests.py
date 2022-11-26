bf6da7b58c0c88dce65d4c6f686e7634a72d1e91
Devansh Chaudhary <devanshchaudhary@Devanshs-MacBook-Pro.local>
2022-11-26 17:59:33 +0530

    fixed linting errors in tests.py and calc.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,6 +8,7 @@ from app import calc
 class CalcTest(SimpleTestCase):
     """Test the Calc module."""
 
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
