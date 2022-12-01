954974d4145a423069c8c5b970408efa68d8378b
Devansh Chaudhary <devanshchaudhary@Devanshs-MacBook-Pro.local>
2022-12-02 04:35:01 +0530

    fixed import models from core

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 """Django admin customization."""
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core import models
+import models
 
 
 class UserAdmin(BaseUserAdmin):
